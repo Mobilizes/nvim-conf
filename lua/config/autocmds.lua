baa077683fa1a236a9640c79290e9ba88fa52b9a
Mobilizes <fernando142311@gmail.com>
2025-03-05 20:22:08 +0700

    feat: remove notebook

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -129,16 +129,3 @@ if copilot_exists then
 end
 
 require("remote-sshfs").setup({})
-require("notebook").setup({
-  -- Whether to insert a blank line at the top of the notebook
-  insert_blank_line = true,
-
-  -- Whether to display the index number of a cell
-  show_index = true,
-
-  -- Whether to display the type of a cell
-  show_cell_type = true,
-
-  -- Style for the virtual text at the top of a cell
-  virtual_text_style = { fg = "lightblue", italic = true },
-})
